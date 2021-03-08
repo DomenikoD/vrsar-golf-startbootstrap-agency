9f2d2c2bbb8651523ea7384c3a6e0599cad6c7df
DomenikoD <domeniko.divjak@gmail.com>
2021-03-08 23:14:20 +0100

    no clients

--- src/templates/top-index.jsx
+++ src/templates/top-index.jsx
@@ -122,7 +122,7 @@ const IndexPage = ({ data, pathContext: { langKey, defaultLang, langTextMap } })
 
   return (
     <>
-      <SEO lang={langKey} title="Top" keywords={keywords} description={description} />
+      <SEO lang={langKey} title="Valkanela" keywords={keywords} description={description} />
       <Navbar
         anchors={anchors}
         frontmatter={navBarNode.frontmatter}
--- src/views/Sections/Clients/Clients-backup.jsx
+++ src/views/Sections/Clients/Clients-backup.jsx
@@ -14,7 +14,15 @@ const Clients = ({ className, frontmatter }) => {
   const { anchor, clients } = frontmatter;
 
   return (
-    <div></div>
+    <PageSection className={clsx("py-5", className)} id={anchor}>
+      <Row>
+        {clients.map(({ href, imageFileName }) => (
+          <Col md={3} sm={6} className="my-3" key={imageFileName}>
+            <Client href={href} imageFileName={imageFileName} />
+          </Col>
+        ))}
+      </Row>
+    </PageSection>
   );
 };
 
