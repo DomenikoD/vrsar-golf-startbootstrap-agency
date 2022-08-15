cf7b7a8a9f1ecba7ebaeb5b5024d6ee20cb04e9d
Domeniko <domeniko.divjak@gmail.com>
2022-08-15 21:53:44 +0200

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,17 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-PH7N39GXNT",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-PH7N39GXNT", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
     {
