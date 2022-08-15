7ae5e914717009aeb7e0251d5ca25c941203e8e4
Domeniko <domeniko.divjak@gmail.com>
2022-08-15 22:56:49 +0200

    nejde gtag stara verziga gtsbya, 4 nova

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,20 +9,20 @@ module.exports = {
     author,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-PH7N39GXNT", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-        },
-      },
-    },
+    //{
+    //resolve: `gatsby-plugin-google-gtag`,
+    //options: {
+    // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //trackingIds: [
+    // "G-PH7N39GXNT", // Google Analytics / GA
+    //],
+    // This object is used for configuration specific to this plugin
+    //pluginConfig: {
+    // Puts tracking script in the head instead of the body
+    //head: true,
+    //},
+    //},
+    //},
     {
       resolve: "gatsby-plugin-manifest",
       options: {
